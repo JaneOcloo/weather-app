9817da38cd3c85906cffadf8dcd124e9ad92037b
JaneOcloo <jane.ocloo1@gmail.com>
2025-01-25 01:05:00 +0300

    added basic js

--- index.js
+++ index.js
@@ -7,10 +7,20 @@ function searchacity(event) {
 let searchFormElement = document.querySelector("#forma");
 searchFormElement.addEventListener("submit", searchacity);
 
+function showUser(response) {
+  alert(`The user name is ${response.data.name}`);
+}
+let city = "Sydney";
+let apiKey = "eafo3c54a06d9f77ba3t96121805c98f";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+axios.get(apiUrl).then(showUser);
+
 function changeDate() {
-  let now = newDate();
-  let day = now.getDate();
-  let minute = now.getMinutes.toString().padStart(2, "0");
+  let now = new Date();
+  let date = now.getDate();
+  let day = now.getDay();
+  let minute = now.getMinutes().toString().padStart(2, "0");
   let hour = now.getHours().toString().padStart(2, "0");
 
   let days = [
@@ -22,7 +32,8 @@ function changeDate() {
     "Friday",
     "Saturday",
   ];
-  let formatDate = `${days[day]} ${hour}:${minute}`;
+  let formatDate = `${days[day]} ${date} ${hour}:${minute}`;
   let dateNow = document.querySelector("#my-date");
   dateNow.innerHTML = formatDate;
 }
+changeDate();
